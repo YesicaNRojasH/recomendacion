43efdd570fdcb22b64fedb8080c66a1672884557
YESICA ROJAS <89708088+YesicaNRojasH@users.noreply.github.com>
2023-11-16 15:25:25 -0500

    Add files via upload

--- api/app.py
+++ api/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
 
 app = Flask(__name__)
 
@@ -19,6 +21,59 @@ def get_top_users():
     top_users = ratings.groupby('userId')['rating'].count().reset_index().sort_values('rating', ascending=False)[:10]
     # Convertir los datos a formato JSON y devolverlos como respuesta
     return jsonify(top_users.to_dict(orient='records'))
+
+@app.route('/api/ratings_distribution', methods=['GET'])
+def get_ratings_distribution():
+    # Cargar el archivo CSV con Pandas
+    ratings = pd.read_csv('ratings.csv')
+
+    # Calcular la distribución de calificaciones
+    data = ratings['rating'].value_counts().sort_index(ascending=False)
+    fig = px.bar(x=data.index, y=data.values, labels={'x': 'Rating', 'y': 'Count'},
+                 title='Distribution Of Ratings')
+
+    # Renderizar la gráfica como HTML
+    plot_html = fig.to_html(full_html=False)
+
+    # Devolver la representación de la gráfica como respuesta
+    return plot_html
+
+@app.route('/api/user_ratings_distribution', methods=['GET'])
+def get_user_ratings_distribution():
+    # Cargar el archivo CSV con Pandas
+    ratings = pd.read_csv('ratings.csv')
+
+    # Calcular la distribución del número de calificaciones por usuario
+    data = ratings.groupby('userId')['rating'].count().clip(upper=50)
+    trace = go.Histogram(x=data.values, xbins=dict(start=0, end=50, size=2),
+                         name='Ratings')
+
+    # Crear layout
+    layout = go.Layout(title='Distribution Of Number of Ratings Per User (Clipped at 50)',
+                       xaxis=dict(title='Number of Ratings Per User'),
+                       yaxis=dict(title='Count'),
+                       bargap=0.2)
+
+    # Crear plot
+    fig = go.Figure(data=[trace], layout=layout)
+
+    # Renderizar la gráfica como HTML
+    plot_html = fig.to_html(full_html=False)
+
+    # Devolver la representación de la gráfica como respuesta
+    return plot_html
+
+@app.route('/api/top_rated_users', methods=['GET'])
+def get_top_rated_users():
+    # Cargar el archivo CSV con Pandas
+    ratings = pd.read_csv('ratings.csv')
+
+    # Calcular el número de calificaciones por usuario y obtener los 10 usuarios con más calificaciones
+    top_users = ratings.groupby('userId')['rating'].count().reset_index().sort_values('rating', ascending=False)[:10]
+
+    # Convertir los datos a formato JSON y devolverlos como respuesta
+    top_users_json = top_users.to_dict(orient='records')
+    return jsonify(top_users_json)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web/app.js
+++ web/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const axios = require('axios');
 const app = express();
 
 app.get('/ratings', async (req, res) => {
     try {
-        const response = await fetch('http://api:5000/api/ratings');
-        const data = await response.json();
+        const response = await axios.get('http://api:5000/api/ratings');
+        const data = response.data;
         res.send(renderData('Ratings', data));
     } catch (error) {
         console.error(error);
@@ -15,8 +15,8 @@ app.get('/ratings', async (req, res) => {
 
 app.get('/top_users', async (req, res) => {
     try {
-        const response = await fetch('http://api:5000/api/top_users');
-        const data = await response.json();
+        const response = await axios.get('http://api:5000/api/top_users');
+        const data = response.data;
         res.send(renderData('Top 10 Users by Ratings', data));
     } catch (error) {
         console.error(error);
@@ -24,21 +24,46 @@ app.get('/top_users', async (req, res) => {
     }
 });
 
+app.get('/top_rated_users', async (req, res) => {
+    try {
+        const response = await axios.get('http://api:5000/api/top_rated_users');
+        const data = response.data;
+        res.send(renderData('Top 10 Users by Number of Ratings', data));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching data');
+    }
+});
+
 function renderData(label, data) {
-    let output = `<h1>${label}</h1>`;
+    let output = `<h1 style="color: #465615; font-size: 24px; text-align: center; margin-top: 50px; border-bottom: 2px solid #A4CB2F;">${label}</h1>`;
     if (data && data.length > 0) {
-        output += '<ul>';
+        output += `
+            <table style="border-collapse: collapse; width: 60%; margin: 0 auto; border: 1px solid #ddd; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); margin-bottom: 20px;">
+                <thead style="background-color: #A4CB2F;">
+                    <tr>
+                        <th style="padding: 12px; text-align: left;">User ID</th>
+                        <th style="padding: 12px; text-align: left;">Rating</th>
+                    </tr>
+                </thead>
+                <tbody>`;
         data.forEach(item => {
             if (item.userId) {
-                output += `<li>User ID: ${item.userId} - Rating: ${item.rating}</li>`;
+                output += `
+                    <tr style="border-bottom: 1px solid #ddd;">
+                        <td style="padding: 8px;">${item.userId}</td>
+                        <td style="padding: 8px;">${item.rating}</td>
+                    </tr>`;
             } else {
                 // Handle other types of data according to the consumed API
                 // ...
             }
         });
-        output += '</ul>';
+        output += `
+                </tbody>
+            </table>`;
     } else {
-        output += 'No data available.';
+        output += '<p style="color: #777; text-align: center;">No data available.</p>';
     }
     return output;
 }
